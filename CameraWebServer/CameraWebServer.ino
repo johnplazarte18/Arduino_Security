9eb6a43d4b1383bd4b7f1cd9ab4a3e9c47468a42
johnplazarte18 <john.plazarte2017@uteq.edu.ec>
2021-08-31 23:25:20 -0500

    loop añadido

--- CameraWebServer/CameraWebServer.ino
+++ CameraWebServer/CameraWebServer.ino
@@ -1,5 +1,13 @@
+#include <HTTPClient.h>
 #include "esp_camera.h"
 #include <WiFi.h>
+#include "base64.h"
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+#include <WiFiClient.h>
+#include <TimeLib.h>
+
+WiFiClient wifiClient;
 
 //
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
@@ -19,13 +27,26 @@
 
 #include "camera_pins.h"
 
-const char* ssid = "FAMILIA PLAZARTE";
-const char* password = "johngary";
+const char* ssid = "UTEQ-DOCENTES";
+const char* password = "docenciauteq";
+
+time_t fecha;
+
+const int motionSensor = 16;
+int idComponente;//lo que se envia
 
 void startCameraServer();
 
 void setup() {
   Serial.begin(115200);
+  pinMode(motionSensor, INPUT_PULLUP);
+  for (int i = 0; i > 30; i++) //Utilizamos un for para calibrar el sensor depende del tipo de sensor que utilicemos va a cambiar el tiempo de calibración
+  {
+    delay(1000);
+  }
+  delay(50);
+
+
   Serial.setDebugOutput(true);
   Serial.println();
 
@@ -50,10 +71,10 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  
+
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   //                      for larger pre-allocated frame buffer.
-  if(psramFound()){
+  if (psramFound()) {
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
@@ -89,8 +110,9 @@ void setup() {
   s->set_vflip(s, 1);
   s->set_hmirror(s, 1);
 #endif
-  
+
   WiFi.begin(ssid, password);
+  Serial.println("Conectando a la red wifi");
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -104,19 +126,158 @@ void setup() {
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
-  camera_fb_t * fb = NULL;
- 
-  // Take Picture with Camera
-  fb = esp_camera_fb_get();  
-  if(!fb) {
-    Serial.println("Camera capture failed");
-    return;
-  }
-  const char *data = (const char *)fb->buf;
-  Serial.println((const char *)fb->buf);
+  setTime(12, 55, 0, 26, 8, 2021);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(10000);
+
+  fecha = now();
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("Conectado ");
+    HTTPClient http;//Instancia de la clase
+
+    //GET---------COMPONENTES
+    bool estadoPir = true;
+    http.begin(wifiClient, "http://wssecurity.herokuapp.com/api-seguridad/componentes/");
+    int httpCodGet = http.GET();
+    if (httpCodGet == 200)
+    {
+      String respuestacomp = http.getString();
+      StaticJsonDocument<192> docComp;
+
+      DeserializationError error = deserializeJson(docComp, respuestacomp);
+
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+
+      JsonObject componente_0 = docComp["componente"][0];
+      estadoPir = componente_0["estado"]; // true
+      Serial.println("El servidor respondio:");
+      Serial.println(respuesta);
+    } else {
+      Serial.println(httpCodGet);
+    }
+
+    //GET---------SOLICITADO
+    http.begin(wifiClient, "https://wssecurity.herokuapp.com/api-seguridad/solicitud/");
+    int httpCodGetSolic = http.GET();
+    if (httpCodGetSolic == 200)
+    {
+      String respuesta = http.getString();
+      StaticJsonDocument<48> doc;
+      DeserializationError error = deserializeJson(doc, respuesta);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+      bool solicitud = doc["solicitud"]; 
+      
+      //+++++++++++++++++++++++++SOLICITADO
+      if (solicitud) {
+        http.begin(wifiClient, "http://wssecurity.herokuapp.com/api-seguridad/historial-anomalias/");
+        http.addHeader("content-type", "application/json");
+        camera_fb_t * fb = NULL;
+
+        fb = esp_camera_fb_get();
+
+        String imgDataB64 = base64::encode(fb->buf, fb->len);
+
+        String jsonSolicitud;
+        DynamicJsonDocument doc(12288);
+
+        doc["componente_id"] = 1;
+        doc["fecha"] = String(year(fecha)) + "-" + String(month(fecha)) + "-" + String(day(fecha));
+        doc["tipo_historial"] = "Solicitado";
+
+        JsonObject evidencias_0 = doc["evidencias"].createNestedObject();
+        evidencias_0["hora"] = String(hour(fecha)) + ":" + String(minute(fecha)) + ":" + String(second(fecha));
+        evidencias_0["foto"] = "data:image/PNG;base64," + imgDataB64;
+
+        serializeJson(doc, jsonSolicitud);
+
+        int httpCodPostSolic = http.POST(jsonSolicitud);
+
+
+        if (httpCodPostSolic == 200)
+        {
+          String respuesta = http.getString();
+          Serial.println("El servidor respondio:");
+          Serial.println(respuesta);
+        } else {
+          Serial.println(httpCodPostSolic);
+        }
+
+        String jsonSolicitudModif;
+        http.begin(wifiClient, "http://wssecurity.herokuapp.com/api-seguridad/solicitud/");
+        http.addHeader("content-type", "application/json");
+
+        StaticJsonDocument<16> docSolicitudModif;
+        docSolicitudModif["estado"] = 0;
+        serializeJson(docSolicitudModif, jsonSolicitudModif);
+        int httpCodPutSolic = http.PUT(jsonSolicitudModif);
+        if (httpCodPutSolic == 200)
+        {
+          String respuesta = http.getString();
+          Serial.println("El servidor respondio:");
+          Serial.println(respuesta);
+        } else {
+          Serial.println(httpCodPutSolic);
+        }
+      }
+    } else {
+      Serial.println(httpCodGetSolic);
+    }
+
+
+    //+++++++++++++++++++++++++ANOMALIA
+    if (estadoPir &&  digitalRead(motionSensor) == HIGH) {
+      idComponente = 1; //LATERAL DE DERECHO
+      Serial.println(idComponente);
+
+      //POST
+      http.begin(wifiClient, "http://wssecurity.herokuapp.com/api-seguridad/historial-anomalias/");
+      http.addHeader("content-type", "application/json");
+
+      if (idComponente > 0) {
+        camera_fb_t * fb = NULL;
+        fb = esp_camera_fb_get();
+
+        String imgDataB64 = base64::encode(fb->buf, fb->len);
+
+        String json;
+        DynamicJsonDocument doc(12288);
+
+        doc["componente_id"] = idComponente;
+        doc["fecha"] = String(year(fecha)) + "-" + String(month(fecha)) + "-" + String(day(fecha));
+        doc["tipo_historial"] = "Anomalía";
+
+        JsonObject evidencias_0 = doc["evidencias"].createNestedObject();
+        evidencias_0["hora"] = String(hour(fecha)) + ":" + String(minute(fecha)) + ":" + String(second(fecha));
+        evidencias_0["foto"] = "data:image/PNG;base64," + imgDataB64;
+
+        Serial.println(imgDataB64);
+        serializeJson(doc, json);
+
+        Serial.println(json);
+        int httpCod = http.POST(json);
+
+          if (httpCod == 200)
+          {
+          String respuesta = http.getString();
+          Serial.println("El servidor respondio:");
+          Serial.println(respuesta);
+          } else {
+          Serial.println(httpCod);
+          }
+      }
+      http.end();
+    }
+  }
+
+  delay(5000);
 }
